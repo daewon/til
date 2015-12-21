ac3b3c34a221c9c971f1cf6762464da5b7e98f25
daewon <blueiur@gmail.com>
2015-12-22 00:36:06 +0900

    Add simple parser

--- scala/learnToScala/src/main/scala/SimpleCalc.scala
+++ scala/learnToScala/src/main/scala/SimpleCalc.scala
@@ -24,29 +24,51 @@ object SimpleCalc {
     def eval(): Double = a.foldLeft(0.0)(_ + _.eval)
   }
 
+  case class EMul(left: Expr, right: Expr) extends Expr {
+    def eval(): Double = left.eval * right.eval
+  }
+
+  case class EDiv(left: Expr, right: Expr) extends Expr {
+    def eval(): Double = left.eval / right.eval
+  }
+
+  case class EUMinus(e: Expr) extends Expr {
+    def eval(): Double = -e.eval
+  }
+
   object ExprParser extends StandardTokenParsers {
-    lexical.delimiters ++= Set("+", "-")
+    lexical.delimiters ++= Set("+", "-", "*", "/", "(", ")")
 
     def value = numericLit ^^ { s => EConst(s.toDouble) }
 
-    def sum = value * (
+    def unaryMinus: Parser[EUMinus] = "-" ~> term ^^ {
+      EUMinus(_)
+    }
+
+    def sum = product * (
       "+" ^^^ { (a: Expr, b: Expr) => EAdd(a, b) } |
         "-" ^^^ { (a: Expr, b: Expr) => ESub(a, b) }
       )
 
-    def expr = sum | value //top level expression
+    def product = term * (
+      "*" ^^^ { (a: Expr, b: Expr) => EMul(a, b) } |
+        "/" ^^^ { (a: Expr, b: Expr) => EDiv(a, b) }
+      )
+
+    def parens = "(" ~> expr <~ ")"
+
+    def term: Parser[Expr] = value | parens | unaryMinus
+
+    def expr = sum | term
 
     def parse(s: String) = {
       val tokens = new lexical.Scanner(s)
       phrase(expr)(tokens)
     }
 
-    def apply(s: String): Expr = {
-      parse(s) match {
-        case Success(tree, _) => tree
-        case e: NoSuccess =>
-          throw new IllegalArgumentException("Bad syntax: " + s)
-      }
+    def apply(s: String): Expr = parse(s) match {
+      case Success(tree, _) => tree
+      case e: NoSuccess => throw new IllegalArgumentException("Bad syntax: " + s)
     }
 
     //Simplify testing
--- scala/learnToScala/src/test/scala/SpmpleCalcSpec.scala
+++ scala/learnToScala/src/test/scala/SpmpleCalcSpec.scala
@@ -5,14 +5,30 @@ import org.specs2.mutable.Specification
 class SimpleCalcSpec extends Specification {
   "SimpleCalc Specifications" >> {
 
-    "1" >> {
+    "const" >> {
       SimpleCalc.parse("1") === EConst(1)
     }
 
-    "1 + 1" >> {
+    "add" >> {
       SimpleCalc.parse("1 + 1") === EAdd(EConst(1.0), EConst(1.0))
       SimpleCalc.parse("1 - 1") === ESub(EConst(1.0), EConst(1.0))
       SimpleCalc.parse("1 - 1 - 3") === ESub(ESub(EConst(1.0), EConst(1.0)), EConst(3.0))
+    }
+
+    "multiply" >> {
+      SimpleCalc.parse("1 * 1") === EMul(EConst(1.0), EConst(1.0))
+      SimpleCalc.parse("1 - 1 * 3") ===
+        ESub(EConst(1.0), EMul(EConst(1.0), EConst(3.0)))
+    }
+
+    "parens" >> {
+      SimpleCalc.parse("(1 - 1) * 3") ===
+        EMul(ESub(EConst(1.0), EConst(1.0)), EConst(3.0))
+    }
+
+    "unary op" >> {
+      SimpleCalc.parse("3 * -(2+2)") ===
+        EMul(EConst(3.0), EUMinus(EAdd(EConst(2.0), EConst(2.0))))
     }
   }
 }
