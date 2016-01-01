eb559346212221dc7d052a91627927ff7bd92d61
daewon <blueiur@gmail.com>
2016-01-01 23:48:59 +0900

    Lambda test

--- scala/nio/src/main/java/JavaMain.java
+++ scala/nio/src/main/java/JavaMain.java
@@ -2,20 +2,27 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousFileChannel;
 import java.nio.channels.CompletionHandler;
-import java.nio.channels.WritePendingException;
-import java.nio.file.*;
-import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
 
 public class JavaMain {
-  public static void main(String[] args) throws IOException, InterruptedException {
+  public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
     System.out.println("Start to learn IO!");
 
     List<String> stooges = Arrays.asList("Larry", "Moe", "Curly");
     stooges.stream().forEach(System.out::println);
 
     Path f = Paths.get("/Users/daewon/code/til/scala/nio/src/main/java/JavaMain.java");
+    CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> "return value");
+    CompletableFuture with = future.thenAccept(s -> System.out.println("Accept previous:: " + s));
+
+    System.out.println(with.get());
 
     AsynchronousFileChannel ch = AsynchronousFileChannel.open(f.toAbsolutePath(), StandardOpenOption.READ);
     ByteBuffer buf = ByteBuffer.allocateDirect(2048);
@@ -31,5 +38,7 @@ public class JavaMain {
       public void failed(Throwable exc, Object attachment) {
       }
     });
+
+    Thread.sleep(100);
   }
 }
