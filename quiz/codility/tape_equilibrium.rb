372b692a86f7fbf1e299a50132eac261f56b940e
daewon <daewon@apache.org>
2016-01-26 00:44:25 +0900

    add tape_equilibrium

---
+++ quiz/codility/tape_equilibrium.rb
@@ -0,0 +1,17 @@
+# you can write to stdout for debugging purposes, e.g.
+# puts "this is a debug message"
+
+def solution(a)
+    # write your code in Ruby 2.2
+    sum = a.inject(:+)
+    acc = 0
+
+    min = 99999999
+    a[0..-2].each do |n|
+        sum -= n
+        acc += n
+
+        min = [(acc - sum).abs, min].min
+    end
+    min
+end
