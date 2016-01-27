4defdbfec31a8637b66cb4fc33b49b56c924d1a8
daewon <daewon@apache.org>
2016-01-27 23:15:29 +0900

    add perm_check

---
+++ quiz/codility/perm_check.rb
@@ -0,0 +1,18 @@
+def solution(a)
+    # write your code in Ruby 2.2
+    return a.first == 1 ? 1 : 0 if a.one?
+    
+    set = {}
+    sum, max, min = 0, -1, 9999999999999999999
+
+    a.each do |n|
+        return 0 if set.include? n
+        
+        set[n] = true
+        max = n if n > max
+        min = n if n < min
+        sum += n
+    end
+    
+    (sum == (min + max) * max / 2) ? 1 : 0
+end
