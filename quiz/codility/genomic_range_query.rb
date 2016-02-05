0c6a362ecdb8088fd36d5bd120d3d98b76e03222
daewon <daewon@apache.org>
2016-02-05 23:37:57 +0900

    fix 0 index bug

--- quiz/codility/genomic_range_query.rb
+++ quiz/codility/genomic_range_query.rb
@@ -2,32 +2,32 @@
 # puts "this is a debug message"
 
 def solution(s, p, q)
-    chm = { 'A' => 1, 'C' => 2, 'G' => 3, 'T' => 4 } # char to int map
-    rng = {} # store { a: {max: -1, min: 9}, {..}, {..} }
-    
-    s.each_char.each_with_index do |ch, i| 
-        rng[ch] = { min: 5,  max: -1 } unless rng[ch] 
-        m = rng[ch]
-        min, max = m[:min], m[:max]
-        
-        min = i if i < min
-        max = i if i > max
-        
-        rng[ch] = { min: min, max: max }
+  # write your code in Ruby 2.2
+  chm = { 'A' => 1, 'C' => 2, 'G' => 3, 'T' => 4 }
+  acc = { 'A' => 0, 'C' => 0, 'G' => 0, 'T' => 0 }
+  map = { 'A' => [], 'C' => [], 'G' => [], 'T' => [] }
+
+  # make prefix_sum
+  s.each_char.with_index do |ch, i|
+    n = chm[ch]
+    acc[ch] += n
+    chm.keys.each { |c| map[c][i] = acc[c] }
+  end
+
+  # puts map.inspect
+
+  # detect minimal charectre in range(p, q)
+  p.zip(q).map do |i, j|
+    next chm[s[i]] if i == j
+
+    c = chm['T']
+    chm.keys.each do |ch|
+      lhs = (i-1 >= 0) ? map[ch][i-1] : 0
+      if lhs < map[ch][j]
+        c = chm[ch]
+        break
+      end
     end
-    # CAGCCTA => [2, 1, 3, 2, 2, 4, 1]
-    # [2, 4], [5, 5], [0, 6]
-    # puts rng.inspect
-    
-    min_map = []    
-    ['A', 'C', 'G', 'T'].each do |ch|
-        (rng[ch][:min]..rng[ch][:max]).each { |i| min_map[i] = ch }
-    end
-    
-    # puts min_map.inspect
-    indexes = p.zip(q).map do |i, j|
-        chm[min_map[j]]
-    end   
-    
-    indexes
+    c
+  end
 end
