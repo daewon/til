bfa1a077ed55449e497afef416a2db2408c11ca5
daewon <daewon@apache.org>
2016-02-11 01:46:20 +0900

    second try

--- quiz/codility/solve/abs_distinct.rb
+++ quiz/codility/solve/abs_distinct.rb
@@ -2,6 +2,32 @@
 # puts "this is a debug message"
 
 def solution(a)
-  uniq = a.uniq { |n| n.abs }
-  uniq.length
+  len = a.length
+
+  i, j, count = 0, len - 1, 0
+  while i <= j do
+    l, r = a[i].abs, a[j].abs
+
+    if l < r
+      while j > 0 and r == a[j].abs
+        j -= 1
+      end
+    elsif l > r
+      while i < len and l == a[i].abs
+        i += 1
+      end
+    elsif l == r
+      while j > 0 and r == a[j].abs
+        j -= 1
+      end
+
+      while i < len and l == a[i].abs
+        i += 1
+      end
+    end
+
+    count += 1
+  end
+
+  count
 end
