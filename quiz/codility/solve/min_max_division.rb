589e8d45d35a8a723709f84f1193fac78d8c6da6
daewon <daewon@apache.org>
2016-03-01 23:46:33 +0900

    fail ...

--- quiz/codility/solve/min_max_division.rb
+++ quiz/codility/solve/min_max_division.rb
@@ -1,17 +1,24 @@
 # you can write to stdout for debugging purposes, e.g.
 # puts "this is a debug message"
 
+# [2, 1, 5, 1, 2, 2, 2]
 def solution(k, m, a)
-  len = a.length
-  ret = 9999999999999999
+  min = 999999999999999
 
-  def traverse(a, i, j, depth)
-    j = i + 1
+  trav = -> a, i, j do
+    if i + j < a.length
+      t1 = a[0...i].inject(0) { |a, c| a + c }
+      t2 = a[i...j].inject(0) { |a, c| a + c }
+      t3 = a[j...a.length].inject(0) { |a, c| a + c }
 
-    t1 = traverse(a, 0+1, 0, 0)
-    t2 = traverse(a, 0+1, 0, 0)
-    t3 = traverse(a, 0, 0+1, 0)
+      # puts [t1, t2, t3].inspect
+      min = [min, [t1, t2, t3].max].min
+      trav.call(a, i+1,j)
+      trav.call(a, i, j+1)
+    end
   end
 
-  traverse(a, 0, 0, 0)
+  trav.call(a, 0, 0)
+
+  min
 end
