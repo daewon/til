7d36dde1df0e007d2c431af5e9a79c7b887b3388
daewon <daewon@apache.org>
2016-03-02 13:01:33 +0900

    still timeout .. T_T

--- quiz/codility/solve/min_max_division.rb
+++ quiz/codility/solve/min_max_division.rb
@@ -3,22 +3,43 @@
 
 # [2, 1, 5, 1, 2, 2, 2]
 def solution(k, m, a)
-  min = 999999999999999
+  @min = 999999999999999
+  @prefix_sum = a.inject([]) { |acc, c| acc << c + (acc.last || 0) }
+  @a = a
+  @k = k
+  @len = a.length
+  @memo = {}
 
-  trav = -> a, i, j do
-    if i + j < a.length
-      t1 = a[0...i].inject(0) { |a, c| a + c }
-      t2 = a[i...j].inject(0) { |a, c| a + c }
-      t3 = a[j...a.length].inject(0) { |a, c| a + c }
+  def trav(idx, p_idx, prev, depth)
+    @memo["#{idx}_#{p_idx}"] if @memo["#{idx}_#{p_idx}"]
+    if depth == @k-1
+      # sum = a[idx...@len].inject(0) { |a, c| a + c }
+      sum =
+        if idx > 0
+          @prefix_sum[@len-1] - @prefix_sum[idx - 1]
+        else
+          @prefix_sum[@len-1]
+        end
 
-      # puts [t1, t2, t3].inspect
-      min = [min, [t1, t2, t3].max].min
-      trav.call(a, i+1,j)
-      trav.call(a, i, j+1)
+      @min = [@min, [prev, sum].max].min
+
+      @memo["#{idx}_#{p_idx}"] = @min
+    else
+      (idx...(@len)).each do |ci|
+        #sum = a[idx...ci].inject(0) { |a, c| a + c }
+        sum =
+          if idx > 0
+            @prefix_sum[ci-1] - @prefix_sum[idx - 1]
+          else
+            @prefix_sum[ci-1]
+          end
+
+        trav(ci, idx, [sum, prev].max, depth + 1)
+      end
     end
   end
 
-  trav.call(a, 0, 0)
+  trav(0, 0, 0, 0)
 
-  min
+  @min
 end
