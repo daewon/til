cd9f8b29474fcc53732e1b956375a4b6ac2cde1f
daewon <daewon@apache.org>
2016-03-09 01:04:43 +0900

    refactor with pattern matching

--- exercism/elixir/list-ops/list_ops.exs
+++ exercism/elixir/list-ops/list_ops.exs
@@ -7,100 +7,42 @@ defmodule ListOps do
   # automatically imported) and so shouldn't be used either.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-    trav = fn(ls, trav) ->
-      case ls do
-        [h | t] -> 1 + trav.(t, trav)
-        [] -> 0
-      end
-    end
-
-    trav.(l, trav)
-  end
+  def count([]), do: 0
+  def count([h | t]), do: 1 + count(t)
 
   @spec reverse(list) :: list
-  def reverse(l) do
-    trav = fn(ls, acc, trav) ->
-      case ls do
-        [h | t] -> trav.(t, [h | acc], trav)
-        [] -> acc
-      end
-    end
-
-    trav.(l, [], trav)
-  end
+  def reverse([]), do: []
+  def reverse(ls), do: reverse(ls, [])
+  def reverse([h | t], acc), do: reverse(t, [h | acc])
+  def reverse([], acc), do: acc
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-    trav = fn(ls, trav) ->
-      case ls do
-        [h | t] -> [f.(h) | trav.(t,trav)]
-        [] -> []
-      end
-    end
-
-    trav.(l, trav)
-  end
+  def map([], _), do: []
+  def map([h | t], f), do: [f.(h) | map(t, f)]
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
-    trav = fn(ls, trav) ->
-      case ls do
-        [h | t] ->
-          case f.(h)  do
-            true -> [h | trav.(t,trav)]
-            _ -> trav.(t,trav)
-          end
-        [] -> []
-      end
+  def filter([], _), do: []
+  def filter([h | t], f) do
+    case f.(h) do
+      true -> [h | filter(t, f)]
+      _ -> filter(t, f)
     end
-
-    trav.(l, trav)
   end
 
   @type acc :: any
   @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
-  def reduce(l, acc, f) do
-    trav = fn(ls, acc, trav) ->
-      case ls do
-        [h | t] -> trav.(t, f.(h, acc), trav)
-        [] -> acc
-      end
-    end
-
-    trav.(l, acc, trav)
-  end
+  def reduce([], acc, _), do: acc
+  def reduce([h | t], acc, f), do: reduce(t, f.(h, acc), f)
 
   @spec append(list, list) :: list
-  def append(a, b) do
-    trav = fn(ls, trav) ->
-      case ls do
-        [h | t] -> [h | trav.(t, trav)]
-        [] -> b
-      end
-    end
-
-    trav.(a, trav)
-  end
+  def append([], []), do: []
+  def append(a, []), do: a
+  def append([], b), do: b
+  def append([h | t], b), do: [h | append(t, b)]
 
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
-    trav = fn(ls, trav) ->
-      lss = cond do
-        is_map(ls) -> List.to_list(ls)
-        true -> ls
-      end
-
-      case lss do
-        [h | t] ->
-          case is_list(h) do
-            true -> append(h, trav.(t, trav))
-            false -> [h | trav.(t, trav)]
-          end
-        [] -> []
-      end
-    end
-
-    trav.(ll, trav)
-  end
+  def concat([]), do: []
+  def concat([h | t]) when is_map(h), do: concat([List.to_list(h) | t])
+  def concat([h | t]) when is_list(h), do: append(h, concat(t))
+  def concat([h | t]), do: [h | concat(t)]
 end
