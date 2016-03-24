d9916b085dc252eed364b6c355319a7dc53cd9bb
daewon <daewon@apache.org>
2016-03-24 09:09:01 +0900

    refactoring for min/max value lookup 0(n) -> 0(1)

--- exercism/elixir/saddle-points/saddle_points.exs
+++ exercism/elixir/saddle-points/saddle_points.exs
@@ -1,4 +1,6 @@
 defmodule Matrix do
+  defp flip_tuple({a, b}), do: {b, a}
+
   @doc """
   Parses a string representation of a matrix
   to a list of rows
@@ -8,11 +10,17 @@ defmodule Matrix do
     str
     |> String.split("\n", trim: true)
     |> Enum.map(&(String.split(&1, " ", trim: true)))
-    |> Enum.map(fn row -> row |> Enum.map(&(String.to_integer(&1))) end)
+    |> Enum.map(fn row -> row |> Enum.map(&String.to_integer/1) end)
   end
+
   def rows(str, with_max: true) do
     rs = rows(str)
-    max_rs = rs |> Enum.map(&Enum.max/1)
+    max_rs = rs
+    |> Enum.map(&Enum.max/1)
+    |> Enum.with_index
+    |> Enum.map(&flip_tuple/1)
+    |> Enum.into(%{})
+
     {rs, max_rs}
   end
 
@@ -21,42 +29,46 @@ defmodule Matrix do
   to a list of columns
   """
   @spec columns(String.t()) :: [[integer]]
-  def columns(str) do
-    str |> rows |> transpose
-  end
+  def columns(str), do: str |> rows |> transpose
   def columns(str, with_min: true) do
     cl = columns(str)
-    min_cl = cl |> Enum.map(&Enum.min/1)
+    min_cl = cl
+    |> Enum.map(&Enum.min/1)
+    |> Enum.with_index
+    |> Enum.map(&flip_tuple/1)
+    |> Enum.into(%{})
+
     {cl, min_cl}
   end
 
   defp transpose([[]|_]), do: []
   defp transpose(matrix) do
-    col = matrix |> Enum.map(fn row -> row |> hd end)
-    rest = matrix |> Enum.map(fn row -> row |> tl end)
+    col = matrix |> Enum.map(&hd/1)
+    rest = matrix |> Enum.map(&tl/1)
+
     [col | transpose(rest)]
   end
 
   @doc """
   Calculates all the saddle points from a string
   representation of a matrix
+
+  It's called a "saddle point" because it is greater than or equal to
+  every element in its row and the less than or equal to every element in
+  its column.
   """
   @spec saddle_points(String.t()) :: [{integer, integer}]
   def saddle_points(str) do
-    {rs, maxes} = rows(str, with_max: true)
-    {cs, mins} = columns(str, with_min: true)
+    {rs, max_row_map} = rows(str, with_max: true)
+    {_cs, min_col_map} = columns(str, with_min: true)
     map = for {r, i} <- Enum.with_index(rs), {c, j} <- Enum.with_index(r), do: {{i, j}, c}
 
     Enum.into(map, %{})
     |> Enum.filter(fn {{r, c}, v} ->
-      is_max = Enum.at(maxes, r) === v
-      is_min = Enum.at(mins, c) === v
-      is_max and is_min
+      valid?(max_row_map, r, v) and valid?(min_col_map, c, v)
     end)
     |> Enum.map(fn({point, _}) -> point end)
   end
 
-  # It's called a "saddle point" because it is greater than or equal to
-  # every element in its row and the less than or equal to every element in
-  # its column.
+  defp valid?(map, k, v), do: map[k] === v
 end
