8ac18254aa86a2b7c3a25cb2daeb613ef75625e3
daewon <daewon@apache.org>
2016-03-29 23:30:13 +0900

    palindrom-product

---
+++ exercism/elixir/pythagorean-triplet/pythagorean_triplet.exs
@@ -0,0 +1,50 @@
+defmodule Triplet do
+
+  @doc """
+  Calculates sum of a given triplet of integers.
+  """
+  @spec sum([non_neg_integer]) :: non_neg_integer
+  def sum(triplet) do
+    triplet |> Enum.sum
+  end
+
+  @doc """
+  Calculates product of a given triplet of integers.
+  """
+  @spec product([non_neg_integer]) :: non_neg_integer
+  def product(triplet) do
+    triplet |> Enum.reduce(1, &Kernel.*/2)
+  end
+
+  @doc """
+  Determines if a given triplet is pythagorean. That is, do the squares of a and b add up to the square of c?
+  """
+  @spec pythagorean?([non_neg_integer]) :: boolean
+  def pythagorean?([a, b, c]) do
+    a * a + b * b == c * c
+  end
+
+  @doc """
+  Generates a list of pythagorean triplets from a given min (or 1 if no min) to a given max.
+  """
+  @spec generate(non_neg_integer, non_neg_integer) :: [list(non_neg_integer)]
+  def generate(min, max) do
+    for a <- min..max, b <- a..max, c <- b..max,
+    triple = [a, b, c],
+    pythagorean?(triple) do
+      triple
+    end
+  end
+
+  @doc """
+  Generates a list of pythagorean triplets from a given min to a given max, whose values add up to a given sum.
+  """
+  @spec generate(non_neg_integer, non_neg_integer, non_neg_integer) :: [list(non_neg_integer)]
+  def generate(min, max, sum) do
+    for a <- min..max, b <- a..max, c <- b..max,
+    triple = [a, b, c],
+    pythagorean?(triple) and sum(triple) == sum do
+      triple
+    end
+  end
+end
