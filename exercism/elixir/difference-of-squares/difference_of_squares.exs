c4f17689dd4111b46c3f2911d165704b6c25415e
daewon <daewon@apache.org>
2016-04-09 15:40:23 +0900

    already solved by ruby

---
+++ exercism/elixir/difference-of-squares/difference_of_squares.exs
@@ -0,0 +1,30 @@
+defmodule Squares do
+  @moduledoc """
+  Calculate sum of squares, square of sums, difference between two sums from 1 to a given end number.
+  """
+
+  @doc """
+  Calculate sum of squares from 1 to a given end number.
+  """
+  @spec sum_of_squares(pos_integer) :: pos_integer
+  def sum_of_squares(number) do
+    1..number |> Enum.map(fn n -> n * n end) |> Enum.sum
+  end
+
+  @doc """
+  Calculate square of sums from 1 to a given end number.
+  """
+  @spec square_of_sums(pos_integer) :: pos_integer
+  def square_of_sums(number) do
+    sum_of_square = 1..number |> Enum.sum
+    sum_of_square * sum_of_square
+  end
+
+  @doc """
+  Calculate difference between sum of squares and square of sums from 1 to a given end number.
+  """
+  @spec difference(pos_integer) :: pos_integer
+  def difference(number) do
+    square_of_sums(number) - sum_of_squares(number)
+  end
+end
