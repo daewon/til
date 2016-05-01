8775dee55c11296f099c2c093807dbc659ea2fbc
daewon <daewon@apache.org>
2016-05-01 22:29:49 +0900

    refactor

--- exercism/elixir/diamond/diamond.exs
+++ exercism/elixir/diamond/diamond.exs
@@ -6,16 +6,16 @@ defmodule Diamond do
   @spec build_shape(char) :: String.t
   def build_shape(?A), do: "A\n"
   def build_shape(letter) do
-    ls = ?A..letter |> Enum.to_list |> Enum.with_index
+    ls = ?A..letter |> Enum.with_index
     sz = length(ls) - 1
 
     top = ls
-    |> Enum.map(fn {ch, ln} ->
-      left_pad = List.duplicate(" ", sz - ln)
-      right_pad = List.duplicate(" ", ln)
+    |> Enum.map(fn {ch, idx} ->
+      left_pad = List.duplicate(" ", sz - idx)
+      right_pad = List.duplicate(" ", idx)
 
-      left = "#{left_pad}#{<<ch>>}#{right_pad}" |> String.split("")
-      right = left |> Enum.reverse |> Enum.drop(2) |> Enum.join("") |> String.rstrip
+      left = "#{left_pad}#{<<ch>>}#{right_pad}" |> String.split("", trim: true)
+      right = left |> Enum.reverse |> Enum.drop(1) |> Enum.join("") |> String.rstrip
 
       "#{left}#{right}"
     end)
