ed35bce0bec977e2dd63a47d140dee92b600371d
daewon <daewon@apache.org>
2016-05-02 22:11:30 +0900

    refactor for scale

--- exercism/scala/robot-name/src/main/scala/RoboName.scala
+++ exercism/scala/robot-name/src/main/scala/RoboName.scala
@@ -1,15 +1,17 @@
 object Robot {
   import scala.util.Random
-  val chars = 'A' to 'Z' toSeq
+  def randomCh = ('A'.toInt + Random.nextInt(26)).toChar
+  def randomDigit = Random.nextInt(10)
 
-  def generateName: String = {
-    val twoCh = Random.shuffle(chars).take(2).mkString("")
-    def num = Random.nextInt(10)
-    s"$twoCh$num$num$num"
+  def generateName(numOfChar: Int = 2, numOfDigit: Int = 3): String = {
+    val chPart = (0 until numOfChar).map(_ => randomCh).mkString("")
+    val digitPart = (0 until numOfDigit).map(_ => randomDigit).mkString("")
+
+    chPart + digitPart
   }
 }
 
 class Robot() {
-  var name: String = Robot.generateName
-  def reset() = name = Robot.generateName
+  var name: String = Robot.generateName()
+  def reset(): Unit = name = Robot.generateName()
 }
