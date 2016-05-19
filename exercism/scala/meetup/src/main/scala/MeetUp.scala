e76022094e1c70aec35ba6df65f8e3d4b3f5c3bb
daewon <daewon@apache.org>
2016-05-20 00:19:20 +0900

    add meetup scala

--- exercism/scala/meetup/src/main/scala/MeetUp.scala
+++ exercism/scala/meetup/src/main/scala/MeetUp.scala
@@ -3,7 +3,6 @@ import java.util.Calendar
 
 object Meetup {
   trait Week
-
   val Sun = Calendar.SUNDAY
   val Mon = Calendar.MONDAY
   val Tue = Calendar.TUESDAY
@@ -14,7 +13,7 @@ object Meetup {
 
   val Days = Stream.continually(Seq(Sun, Mon, Tue, Wed, Thu, Fri, Sat)).flatten
 
-  def calendar(year: Int, month: Int, day: Int): Calendar = {
+  def newCalendar(year: Int, month: Int, day: Int): Calendar = {
     val c = Calendar.getInstance()
     c.set(year, month-1, day)
     c
@@ -22,10 +21,13 @@ object Meetup {
 
   def dayOfWeek(c: Calendar): Int = c.get(Calendar.DAY_OF_WEEK);
 
-  def days(c: Calendar): Seq[Int] =
-    1 to c.getActualMaximum(Calendar.DAY_OF_MONTH)
+  def days(c: Calendar): Seq[Int] = 1 to c.getActualMaximum(Calendar.DAY_OF_MONTH)
 
   def daysPair(c: Calendar) = days(c) zip Days.drop(dayOfWeek(c)-1)
+
+  val validTeenth = 13 to 19 toSet // Magic number. I don't know why it's valid. I just passed test cases
+
+  def validDays(cal: Calendar, w: Int) = daysPair(cal).filter { case (_, day) => day == w }.map(_._1)
 
   def apply(m: Int, y: Int) = new Meetup(m, y)
 }
@@ -33,27 +35,19 @@ object Meetup {
 class Meetup(m: Int, y: Int) {
   import Meetup._
 
-  val cal = calendar(y, m, 1)
+  val c = newCalendar(y, m, 1)
 
-  def targets(w: Int) = daysPair(cal).filter { case (_, day) => day == w }
+  def makeCalendar(day: => Int) = new GregorianCalendar(y, m-1, day)
 
-  def teenth(w: Integer) = {
-    new GregorianCalendar(y, m-1, 13)
-  }
+  def teenth(w: Integer) = makeCalendar(validDays(c, w).filter { d => validTeenth(d) }.head)
 
-  def first(w: Int) = {
-    val day = targets(w).head._1
-    new GregorianCalendar(y, m-1, day)
-  }
+  def first(w: Int) = makeCalendar(validDays(c, w).head)
 
-  def second(w: Int) = {
-    val day = targets(w).drop(1).head._1
-    new GregorianCalendar(y, m-1, day)
-  }
+  def second(w: Int) = makeCalendar(validDays(c, w)(1))
 
-  def last(w: Int) = {
-    val day = targets(w).last._1
-    new GregorianCalendar(y, m-1, day)
-  }
+  def third(w: Int) = makeCalendar(validDays(c, w)(2))
 
+  def fourth(w: Int) = makeCalendar(validDays(c, w)(3))
+
+  def last(w: Int) = makeCalendar(validDays(c, w)last)
 }
--- exercism/scala/meetup/src/test/scala/meetup_test.scala
+++ exercism/scala/meetup/src/test/scala/meetup_test.scala
@@ -3,108 +3,90 @@ import java.util.GregorianCalendar
 
 class MeetupTest extends FunSuite with Matchers {
   //Note: Java uses 0-indexed months for GregorianCalendar
-  // test ("monteenth of may 2013") {
-  //   Meetup(5, 2013).teenth(Meetup.Mon) should be (new GregorianCalendar(2013, 4, 13))
-  // }
 
-  // test ("monteenth of august 2013") {
-  //   Meetup(8, 2013).teenth(Meetup.Mon) should be (new GregorianCalendar(2013, 7, 19))
-  // }
+  test ("monteenth of may 2013") {
+    Meetup(5, 2013).teenth(Meetup.Mon) should be (new GregorianCalendar(2013, 4, 13))
+  }
 
-  // test ("monteenth of september 2013") {
-  //   pending
-  //   Meetup(9, 2013).teenth(Meetup.Mon) should be (new GregorianCalendar(2013, 8, 16))
-  // }
+  test ("monteenth of august 2013") {
+    Meetup(8, 2013).teenth(Meetup.Mon) should be (new GregorianCalendar(2013, 7, 19))
+  }
 
-  // test ("tuesteenth of march 2013") {
-  //   pending
-  //   Meetup(3, 2013).teenth(Meetup.Tue) should be (new GregorianCalendar(2013, 2, 19))
-  // }
+  test ("monteenth of september 2013") {
+    Meetup(9, 2013).teenth(Meetup.Mon) should be (new GregorianCalendar(2013, 8, 16))
+  }
 
-  // test ("tuesteenth of april 2013") {
-  //   pending
-  //   Meetup(4, 2013).teenth(Meetup.Tue) should be (new GregorianCalendar(2013, 3, 16))
-  // }
+  test ("tuesteenth of march 2013") {
+    Meetup(3, 2013).teenth(Meetup.Tue) should be (new GregorianCalendar(2013, 2, 19))
+  }
 
-  // test ("tuesteenth of august 2013") {
-  //   pending
-  //   Meetup(8, 2013).teenth(Meetup.Tue) should be (new GregorianCalendar(2013, 7, 13))
-  // }
+  test ("tuesteenth of april 2013") {
+    Meetup(4, 2013).teenth(Meetup.Tue) should be (new GregorianCalendar(2013, 3, 16))
+  }
 
-  // test ("wednesteenth of january 2013") {
-  //   pending
-  //   Meetup(1, 2013).teenth(Meetup.Wed) should be (new GregorianCalendar(2013, 0, 16))
-  // }
+  test ("tuesteenth of august 2013") {
+    Meetup(8, 2013).teenth(Meetup.Tue) should be (new GregorianCalendar(2013, 7, 13))
+  }
 
-  // test ("wednesteenth of february 2013") {
-  //   pending
-  //   Meetup(2, 2013).teenth(Meetup.Wed) should be (new GregorianCalendar(2013, 1, 13))
-  // }
+  test ("wednesteenth of january 2013") {
+    Meetup(1, 2013).teenth(Meetup.Wed) should be (new GregorianCalendar(2013, 0, 16))
+  }
 
-  // test ("wednesteenth of june 2013") {
-  //   pending
-  //   Meetup(6, 2013).teenth(Meetup.Wed) should be (new GregorianCalendar(2013, 5, 19))
-  // }
+  test ("wednesteenth of february 2013") {
+    Meetup(2, 2013).teenth(Meetup.Wed) should be (new GregorianCalendar(2013, 1, 13))
+  }
 
-  // test ("thursteenth of may 2013") {
-  //   pending
-  //   Meetup(5, 2013).teenth(Meetup.Thu) should be (new GregorianCalendar(2013, 4, 16))
-  // }
+  test ("wednesteenth of june 2013") {
+    Meetup(6, 2013).teenth(Meetup.Wed) should be (new GregorianCalendar(2013, 5, 19))
+  }
 
-  // test ("thursteenth of june 2013") {
-  //   pending
-  //   Meetup(6, 2013).teenth(Meetup.Thu) should be (new GregorianCalendar(2013, 5, 13))
-  // }
+  test ("thursteenth of may 2013") {
+    Meetup(5, 2013).teenth(Meetup.Thu) should be (new GregorianCalendar(2013, 4, 16))
+  }
 
-  // test ("thursteenth of september 2013") {
-  //   pending
-  //   Meetup(9, 2013).teenth(Meetup.Thu) should be (new GregorianCalendar(2013, 8, 19))
-  // }
+  test ("thursteenth of june 2013") {
+    Meetup(6, 2013).teenth(Meetup.Thu) should be (new GregorianCalendar(2013, 5, 13))
+  }
 
-  // test ("friteenth of april 2013") {
-  //   pending
-  //   Meetup(4, 2013).teenth(Meetup.Fri) should be (new GregorianCalendar(2013, 3, 19))
-  // }
+  test ("thursteenth of september 2013") {
+    Meetup(9, 2013).teenth(Meetup.Thu) should be (new GregorianCalendar(2013, 8, 19))
+  }
 
-  // test ("friteenth of august 2013") {
-  //   pending
-  //   Meetup(8, 2013).teenth(Meetup.Fri) should be (new GregorianCalendar(2013, 7, 16))
-  // }
+  test ("friteenth of april 2013") {
+    Meetup(4, 2013).teenth(Meetup.Fri) should be (new GregorianCalendar(2013, 3, 19))
+  }
 
-  // test ("friteenth of september 2013") {
-  //   pending
-  //   Meetup(9, 2013).teenth(Meetup.Fri) should be (new GregorianCalendar(2013, 8, 13))
-  // }
+  test ("friteenth of august 2013") {
+    Meetup(8, 2013).teenth(Meetup.Fri) should be (new GregorianCalendar(2013, 7, 16))
+  }
 
-  // test ("saturteenth of february 2013") {
-  //   pending
-  //   Meetup(2, 2013).teenth(Meetup.Sat) should be (new GregorianCalendar(2013, 1, 16))
-  // }
+  test ("friteenth of september 2013") {
+    Meetup(9, 2013).teenth(Meetup.Fri) should be (new GregorianCalendar(2013, 8, 13))
+  }
 
-  // test ("saturteenth of april 2013") {
-  //   pending
-  //   Meetup(4, 2013).teenth(Meetup.Sat) should be (new GregorianCalendar(2013, 3, 13))
-  // }
+  test ("saturteenth of february 2013") {
+    Meetup(2, 2013).teenth(Meetup.Sat) should be (new GregorianCalendar(2013, 1, 16))
+  }
 
-  // test ("saturteenth of october 2013") {
-  //   pending
-  //   Meetup(10, 2013).teenth(Meetup.Sat) should be (new GregorianCalendar(2013, 9, 19))
-  // }
+  test ("saturteenth of april 2013") {
+    Meetup(4, 2013).teenth(Meetup.Sat) should be (new GregorianCalendar(2013, 3, 13))
+  }
 
-  // test ("sunteenth of map 2013") {
-  //   pending
-  //   Meetup(5, 2013).teenth(Meetup.Sun) should be (new GregorianCalendar(2013, 4, 19))
-  // }
+  test ("saturteenth of october 2013") {
+    Meetup(10, 2013).teenth(Meetup.Sat) should be (new GregorianCalendar(2013, 9, 19))
+  }
 
-  // test ("sunteenth of june 2013") {
-  //   pending
-  //   Meetup(6, 2013).teenth(Meetup.Sun) should be (new GregorianCalendar(2013, 5, 16))
-  // }
+  test ("sunteenth of map 2013") {
+    Meetup(5, 2013).teenth(Meetup.Sun) should be (new GregorianCalendar(2013, 4, 19))
+  }
 
-  // test ("sunteenth of october 2013") {
-  //   pending
-  //   Meetup(10, 2013).teenth(Meetup.Sun) should be (new GregorianCalendar(2013, 9, 13))
-  // }
+  test ("sunteenth of june 2013") {
+    Meetup(6, 2013).teenth(Meetup.Sun) should be (new GregorianCalendar(2013, 5, 16))
+  }
+
+  test ("sunteenth of october 2013") {
+    Meetup(10, 2013).teenth(Meetup.Sun) should be (new GregorianCalendar(2013, 9, 13))
+  }
 
   test ("first monday of march 2013") {
     Meetup(3, 2013).first(Meetup.Mon) should be (new GregorianCalendar(2013, 2, 4))
@@ -218,145 +200,117 @@ class MeetupTest extends FunSuite with Matchers {
     Meetup(4, 2013).second(Meetup.Sun) should be (new GregorianCalendar(2013, 3, 14))
   }
 
-  // test ("third monday of march 2013") {
-  //   pending
-  //   Meetup(3, 2013).third(Meetup.Mon) should be (new GregorianCalendar(2013, 2, 18))
-  // }
+  test ("third monday of march 2013") {
+    Meetup(3, 2013).third(Meetup.Mon) should be (new GregorianCalendar(2013, 2, 18))
+  }
 
-  // test ("third monday of april 2013") {
-  //   pending
-  //   Meetup(4, 2013).third(Meetup.Mon) should be (new GregorianCalendar(2013, 3, 15))
-  // }
+  test ("third monday of april 2013") {
+    Meetup(4, 2013).third(Meetup.Mon) should be (new GregorianCalendar(2013, 3, 15))
+  }
 
-  // test ("third tuesday of may 2013") {
-  //   pending
-  //   Meetup(5, 2013).third(Meetup.Tue) should be (new GregorianCalendar(2013, 4, 21))
-  // }
+  test ("third tuesday of may 2013") {
+    Meetup(5, 2013).third(Meetup.Tue) should be (new GregorianCalendar(2013, 4, 21))
+  }
 
-  // test ("third tuesday of june 2013") {
-  //   pending
-  //   Meetup(6, 2013).third(Meetup.Tue) should be (new GregorianCalendar(2013, 5, 18))
-  // }
+  test ("third tuesday of june 2013") {
+    Meetup(6, 2013).third(Meetup.Tue) should be (new GregorianCalendar(2013, 5, 18))
+  }
 
-  // test ("third wednesday of july 2013") {
-  //   pending
-  //   Meetup(7, 2013).third(Meetup.Wed) should be (new GregorianCalendar(2013, 6, 17))
-  // }
+  test ("third wednesday of july 2013") {
+    Meetup(7, 2013).third(Meetup.Wed) should be (new GregorianCalendar(2013, 6, 17))
+  }
 
-  // test ("third wednesday of august 2013") {
-  //   pending
-  //   Meetup(8, 2013).third(Meetup.Wed) should be (new GregorianCalendar(2013, 7, 21))
-  // }
+  test ("third wednesday of august 2013") {
+    Meetup(8, 2013).third(Meetup.Wed) should be (new GregorianCalendar(2013, 7, 21))
+  }
 
-  // test ("third thursday of september 2013") {
-  //   pending
-  //   Meetup(9, 2013).third(Meetup.Thu) should be (new GregorianCalendar(2013, 8, 19))
-  // }
+  test ("third thursday of september 2013") {
+    Meetup(9, 2013).third(Meetup.Thu) should be (new GregorianCalendar(2013, 8, 19))
+  }
 
-  // test ("third thursday of october 2013") {
-  //   pending
-  //   Meetup(10, 2013).third(Meetup.Thu) should be (new GregorianCalendar(2013, 9, 17))
-  // }
+  test ("third thursday of october 2013") {
+    Meetup(10, 2013).third(Meetup.Thu) should be (new GregorianCalendar(2013, 9, 17))
+  }
 
-  // test ("third friday of november 2013") {
-  //   pending
-  //   Meetup(11, 2013).third(Meetup.Fri) should be (new GregorianCalendar(2013, 10, 15))
-  // }
+  test ("third friday of november 2013") {
+    Meetup(11, 2013).third(Meetup.Fri) should be (new GregorianCalendar(2013, 10, 15))
+  }
 
-  // test ("third friday of december 2013") {
-  //   pending
-  //   Meetup(12, 2013).third(Meetup.Fri) should be (new GregorianCalendar(2013, 11, 20))
-  // }
+  test ("third friday of december 2013") {
+    Meetup(12, 2013).third(Meetup.Fri) should be (new GregorianCalendar(2013, 11, 20))
+  }
 
-  // test ("third saturday of january 2013") {
-  //   pending
-  //   Meetup(1, 2013).third(Meetup.Sat) should be (new GregorianCalendar(2013, 0, 19))
-  // }
+  test ("third saturday of january 2013") {
+    Meetup(1, 2013).third(Meetup.Sat) should be (new GregorianCalendar(2013, 0, 19))
+  }
 
-  // test ("third saturday of february 2013") {
-  //   pending
-  //   Meetup(2, 2013).third(Meetup.Sat) should be (new GregorianCalendar(2013, 1, 16))
-  // }
+  test ("third saturday of february 2013") {
+    Meetup(2, 2013).third(Meetup.Sat) should be (new GregorianCalendar(2013, 1, 16))
+  }
 
-  // test ("third sunday of march 2013") {
-  //   pending
-  //   Meetup(3, 2013).third(Meetup.Sun) should be (new GregorianCalendar(2013, 2, 17))
-  // }
+  test ("third sunday of march 2013") {
+    Meetup(3, 2013).third(Meetup.Sun) should be (new GregorianCalendar(2013, 2, 17))
+  }
 
-  // test ("third sunday of april 2013") {
-  //   pending
-  //   Meetup(4, 2013).third(Meetup.Sun) should be (new GregorianCalendar(2013, 3, 21))
-  // }
+  test ("third sunday of april 2013") {
+    Meetup(4, 2013).third(Meetup.Sun) should be (new GregorianCalendar(2013, 3, 21))
+  }
 
-  // test ("fourth monday of march 2013") {
-  //   pending
-  //   Meetup(3, 2013).fourth(Meetup.Mon) should be (new GregorianCalendar(2013, 2, 25))
-  // }
+  test ("fourth monday of march 2013") {
+    Meetup(3, 2013).fourth(Meetup.Mon) should be (new GregorianCalendar(2013, 2, 25))
+  }
 
-  // test ("fourth monday of april 2013") {
-  //   pending
-  //   Meetup(4, 2013).fourth(Meetup.Mon) should be (new GregorianCalendar(2013, 3, 22))
-  // }
+  test ("fourth monday of april 2013") {
+    Meetup(4, 2013).fourth(Meetup.Mon) should be (new GregorianCalendar(2013, 3, 22))
+  }
 
-  // test ("fourth tuesday of may 2013") {
-  //   pending
-  //   Meetup(5, 2013).fourth(Meetup.Tue) should be (new GregorianCalendar(2013, 4, 28))
-  // }
+  test ("fourth tuesday of may 2013") {
+    Meetup(5, 2013).fourth(Meetup.Tue) should be (new GregorianCalendar(2013, 4, 28))
+  }
 
-  // test ("fourth tuesday of june 2013") {
-  //   pending
-  //   Meetup(6, 2013).fourth(Meetup.Tue) should be (new GregorianCalendar(2013, 5, 25))
-  // }
+  test ("fourth tuesday of june 2013") {
+    Meetup(6, 2013).fourth(Meetup.Tue) should be (new GregorianCalendar(2013, 5, 25))
+  }
 
-  // test ("fourth wednesday of july 2013") {
-  //   pending
-  //   Meetup(7, 2013).fourth(Meetup.Wed) should be (new GregorianCalendar(2013, 6, 24))
-  // }
+  test ("fourth wednesday of july 2013") {
+    Meetup(7, 2013).fourth(Meetup.Wed) should be (new GregorianCalendar(2013, 6, 24))
+  }
 
-  // test ("fourth wednesday of august 2013") {
-  //   pending
-  //   Meetup(8, 2013).fourth(Meetup.Wed) should be (new GregorianCalendar(2013, 7, 28))
-  // }
+  test ("fourth wednesday of august 2013") {
+    Meetup(8, 2013).fourth(Meetup.Wed) should be (new GregorianCalendar(2013, 7, 28))
+  }
 
-  // test ("fourth thursday of september 2013") {
-  //   pending
-  //   Meetup(9, 2013).fourth(Meetup.Thu) should be (new GregorianCalendar(2013, 8, 26))
-  // }
+  test ("fourth thursday of september 2013") {
+    Meetup(9, 2013).fourth(Meetup.Thu) should be (new GregorianCalendar(2013, 8, 26))
+  }
 
-  // test ("fourth thursday of october 2013") {
-  //   pending
-  //   Meetup(10, 2013).fourth(Meetup.Thu) should be (new GregorianCalendar(2013, 9, 24))
-  // }
+  test ("fourth thursday of october 2013") {
+    Meetup(10, 2013).fourth(Meetup.Thu) should be (new GregorianCalendar(2013, 9, 24))
+  }
 
-  // test ("fourth friday of november 2013") {
-  //   pending
-  //   Meetup(11, 2013).fourth(Meetup.Fri) should be (new GregorianCalendar(2013, 10, 22))
-  // }
+  test ("fourth friday of november 2013") {
+    Meetup(11, 2013).fourth(Meetup.Fri) should be (new GregorianCalendar(2013, 10, 22))
+  }
 
-  // test ("fourth friday of december 2013") {
-  //   pending
-  //   Meetup(12, 2013).fourth(Meetup.Fri) should be (new GregorianCalendar(2013, 11, 27))
-  // }
+  test ("fourth friday of december 2013") {
+    Meetup(12, 2013).fourth(Meetup.Fri) should be (new GregorianCalendar(2013, 11, 27))
+  }
 
-  // test ("fourth saturday of january 2013") {
-  //   pending
-  //   Meetup(1, 2013).fourth(Meetup.Sat) should be (new GregorianCalendar(2013, 0, 26))
-  // }
+  test ("fourth saturday of january 2013") {
+    Meetup(1, 2013).fourth(Meetup.Sat) should be (new GregorianCalendar(2013, 0, 26))
+  }
 
-  // test ("fourth saturday of february 2013") {
-  //   pending
-  //   Meetup(2, 2013).fourth(Meetup.Sat) should be (new GregorianCalendar(2013, 1, 23))
-  // }
+  test ("fourth saturday of february 2013") {
+    Meetup(2, 2013).fourth(Meetup.Sat) should be (new GregorianCalendar(2013, 1, 23))
+  }
 
-  // test ("fourth sunday of march 2013") {
-  //   pending
-  //   Meetup(3, 2013).fourth(Meetup.Sun) should be (new GregorianCalendar(2013, 2, 24))
-  // }
+  test ("fourth sunday of march 2013") {
+    Meetup(3, 2013).fourth(Meetup.Sun) should be (new GregorianCalendar(2013, 2, 24))
+  }
 
-  // test ("fourth sunday of april 2013") {
-  //   pending
-  //   Meetup(4, 2013).fourth(Meetup.Sun) should be (new GregorianCalendar(2013, 3, 28))
-  // }
+  test ("fourth sunday of april 2013") {
+    Meetup(4, 2013).fourth(Meetup.Sun) should be (new GregorianCalendar(2013, 3, 28))
+  }
 
   test ("last monday of march 2013") {
     Meetup(3, 2013).last(Meetup.Mon) should be (new GregorianCalendar(2013, 2, 25))
