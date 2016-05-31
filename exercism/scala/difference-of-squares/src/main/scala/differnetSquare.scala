6d16eafee018748eaa53059e744e251f0599ad10
daewon <daewon@apache.org>
2016-06-01 00:08:15 +0900

    remove

--- exercism/scala/difference-of-squares/src/main/scala/differnetSquare.scala
+++
@@ -1,8 +0,0 @@
-case class Squares() {
-  def sumOfSquares(n: Int): Int =
-    1 to n map { n => Math.pow(n, 2).toInt } sum
-
-  def squareOfSums(n: Int): Int = Math.pow(1 to n sum, 2).toInt
-
-  def difference(n: Int): Int = squareOfSums(n) - sumOfSquares(n)
-}
