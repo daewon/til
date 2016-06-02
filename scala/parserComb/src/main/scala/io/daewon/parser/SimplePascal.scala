f73a93349d2016991105abd427ce81161844204f
daewon <daewon@apache.org>
2016-06-03 00:48:50 +0900

    add parser

--- scala/parserComb/src/main/scala/io/daewon/parser/SimplePascal.scala
+++ scala/parserComb/src/main/scala/io/daewon/parser/SimplePascal.scala
@@ -2,29 +2,52 @@ package io.daewon.parser
 
 import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 
-/**
-  * program main;
-  *
-  * var int: integer;
-  * var bool: boolean;
-  *
-  * begin
-  *   int = 10;
-  *   bool = false;
-  * end
-  *
-  *.
-  */
+
 object SimplePascal extends StandardTokenParsers {
-  lexical.delimiters ++= Set(";")
-  def program = programHeading ~ block ~ "."
+  //  lexical.delimiters ++= Set(";")
+
+  def program = programHeading ~ identifier ~ "."
 
   def programHeading = "program" ~ identifier ~ ";"
 
-  def identifier = rep1("rep")
+  def block = declarationPart ~ statementPart
 
-  def block = ""
+  def declarationPart = opt(variableDeclarationPart)
+
+  def variableDeclarationPart = "var" ~ rep(variableDeclaration ~ ";")
+
+  def variableDeclaration = identifier ~ ":" ~ typeAnnotation
+
+  def statementPart = "begin" ~ statementSequence ~ "end"
+
+  def statementSequence = assignment ~ ";"
+
+  def assignment = identifier ~ ":=" ~ (number | boolean)
+
+  def identifier = rep1(letter)
+
+  def letter = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
+
+  def typeAnnotation = "integer" | "boolean"
+
+  def number = rep1(digit)
+
+  def digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+
+  def boolean = "true" | "false"
 }
 
-class SimplePascal {
+object SimplePascalParser {
+  val program =
+    """
+   program main;
+
+   var int: integer;
+   var bool: boolean;
+
+   begin
+     int = 10;
+     bool = false;
+   end.
+    """
 }
