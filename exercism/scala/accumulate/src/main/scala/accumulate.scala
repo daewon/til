8600341a6203843ef6ed31cddbe2e58190522cb0
daewon <daewon@apache.org>
2016-06-02 23:27:11 +0900

    apply @tailrec

--- exercism/scala/accumulate/src/main/scala/accumulate.scala
+++ exercism/scala/accumulate/src/main/scala/accumulate.scala
@@ -1,6 +1,12 @@
+import scala.annotation.tailrec
+
 class Accumulate {
-  def accumulate[A, B](f: A => B, ls: Seq[A]): Seq[B] = ls match {
-    case Nil => Nil
-    case head +: tail => f(head) +: accumulate(f, tail)
+  def accumulate[A, B](f: A => B, ls: List[A]): List[B] = {
+    @tailrec def inner(ls: List[A], acc: List[B] = Nil): List[B] = ls match {
+      case Nil => acc
+      case head :: tail => inner(tail, f(head) :: acc)
+    }
+
+    inner(ls).reverse
   }
 }
