a10764cc5085c3be0f456e66294d2747a9a8bd03
daewon <daewon@apache.org>
2016-06-06 15:24:29 +0900

    add raindrop with semigroup typeclass

--- exercism/scala/raindrops/src/main/scala/raindrops.scala
+++ exercism/scala/raindrops/src/main/scala/raindrops.scala
@@ -1,11 +1,38 @@
+trait Semigroup[A] {
+  def append(a: A, b: A): A
+}
+
+object Semigroup {
+  implicit val StringSemigroup = new Semigroup[String] {
+    def append(a: String, b: String) = a + b
+  }
+}
+
+trait Monoid[A] extends Semigroup[A] {
+  def zero: A
+}
+
+object Monoid {
+  implicit def optionMonoid[A](implicit ev: Semigroup[A]) = new Monoid[Option[A]] {
+    def zero : Option[A] = None
+    def append(a : Option[A], b: Option[A]) : Option[A] =
+      (a, b) match {
+        case (Some(a1), Some(b1)) => Some(ev.append(a1, b1))
+        case (Some(_), _) => a
+        case (_, Some(_)) => b
+        case _ => zero
+      }
+  }
+}
+
 case class Raindrops() {
-  def convert(n: Int): String = {
-    var out = ""
+  import Monoid._
 
-    if ((n % 3) == 0) out += "Pling"
-    if ((n % 5) == 0) out += "Plang"
-    if ((n % 7) == 0) out += "Plong"
+  def convert(n: Int)(implicit ev: Monoid[Option[String]]): String = {
+    val pling = if (n % 3 == 0) Option("Pling") else None
+    val plang = if (n % 5 == 0) Option("Plang") else None
+    val plong = if (n % 7 == 0) Option("Plong") else None
 
-    if (out == "") n.toString else out
+    Seq(pling, plang, plong).reduce[Option[String]](ev.append).getOrElse(n.toString)
   }
 }
