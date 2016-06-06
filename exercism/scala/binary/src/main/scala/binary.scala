1392d89c2a7eec303a7bb1649d030edb905e5062
daewon <daewon@apache.org>
2016-06-06 10:14:07 +0900

    refactoring functional version

--- exercism/scala/binary/src/main/scala/binary.scala
+++ exercism/scala/binary/src/main/scala/binary.scala
@@ -2,16 +2,10 @@ object Binary {
   def apply(num: String) = new Binary(num)
 }
 
-class Binary(_num: String) {
-  lazy val digits: Seq[String] = _num.split("")
-
-  lazy val isValid: Boolean = digits.forall(c => c == "0" || c == "1")
-
-  lazy val validDigits: Seq[Int] = if (isValid) digits.map(_.toInt) else Nil
-
-  lazy val toDecimal: Int = validDigits
-    .reverse
-    .zipWithIndex
-    .map { case (n, base) => n * Math.pow(2, base).toInt }
-    .sum
+class Binary(num: String) {
+  def toDecimal: Int = num.foldLeft(Some(0): Option[Int]) {
+    case (Some(n), '0') => Some(n << 1)
+    case (Some(n), '1') => Some((n << 1) + 1)
+    case _ => None
+  } getOrElse 0
 }
