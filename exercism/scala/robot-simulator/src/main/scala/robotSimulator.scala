16dffa7095121181f51525e4e8d9f0160211ba98
daewon <daewon@apache.org>
2016-06-11 00:17:58 +0900

    add simulator

--- exercism/scala/robot-simulator/src/main/scala/robotSimulator.scala
+++ exercism/scala/robot-simulator/src/main/scala/robotSimulator.scala
@@ -6,16 +6,29 @@ object Bearing {
   case object South extends Dir
 }
 
-object Robot {
-  import Bearing._
-}
-
 case class Robot(bearing: Bearing.Dir, coordinates: (Int, Int)) {
   import Robot._
   import Bearing._
 
-  def advance = copy(bearing = bearing, coordinates = (coordinates._1, coordinates._2))
-  def turnRight = ???
-  def turnleft = ???
-  def simulate(in: String) = ???
+  def advance = bearing match {
+    case North => copy(coordinates = (coordinates._1, coordinates._2 + 1))
+    case East => copy(coordinates = (coordinates._1 + 1, coordinates._2))
+    case South => copy(coordinates = (coordinates._1, coordinates._2 - 1))
+    case West => copy(coordinates = (coordinates._1 - 1, coordinates._2))
+  }
+
+  def turnRight() = bearing match {
+    case North => copy(bearing = East)
+    case East => copy(bearing = South)
+    case South => copy(bearing = West)
+    case West => copy(bearing = North)
+  }
+
+  def turnLeft() = turnRight.turnRight.turnRight
+
+  def simulate(in: String) = in.foldLeft(this) {
+    case (acc, 'A') => acc.advance
+    case (acc, 'L') => acc.turnLeft
+    case (acc, 'R') => acc.turnRight
+  }
 }
