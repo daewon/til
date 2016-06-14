4ee5d42449c23a93d80edaf206c76f005f0f24e3
daewon <daewon@apache.org>
2016-06-15 00:11:03 +0900

    add word count

--- exercism/ruby/word-count/word_count.rb
+++ exercism/ruby/word-count/word_count.rb
@@ -1,17 +1,15 @@
 class Phrase
   def initialize(s)
-    @sentence = s
+    @sentence = s.downcase
   end
+
   def word_count
-    map = {}
-    @sentence.(/[:\s]/).each do |word|
+    @sentence.scan(/[a-z0-9]+[']?[a-z]+|\d+/).flatten.each_with_object({}) do |word, map|
       if map[word]
         map[word] += 1
       else
         map[word] = 1
       end
     end
-
-    map
   end
 end
