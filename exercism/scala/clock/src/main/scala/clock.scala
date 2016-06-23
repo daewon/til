2f2f686b791f849dd00eeb8285cf52913960adaf
daewon <daewon@apache.org>
2016-06-23 22:20:17 +0900

    complete clock

--- exercism/scala/clock/src/main/scala/clock.scala
+++ exercism/scala/clock/src/main/scala/clock.scala
@@ -1,23 +1,15 @@
 object Clock {
   def padRight(in: String, n: Int = 2): String = in.reverse.padTo(n, "0").mkString("").reverse
 
-  def apply(_h: Int, _m: Int): Clock = {
-    val (hp, m) = (_m / 60, _m % 60)
-    println(hp -> m)
+  def apply(h: Int, m: Int): Clock = {
+    val (carry, min) =
+      if (m < 0) m / 60 - 1 -> (m % 60 + 60)
+      else m / 60 -> m % 60
 
-    if (_h < 0) {
-      val h = hp + _h
-      new Clock(h % 24 + 24 , m)
-    } else {
-      val h = hp + _h
-      new Clock(h % 24 , m)
-    }
+    val hour = carry + h
 
-    // val h = if (_h < 0) _h % 24 + 24 else _h % 24
-    // val m = if (_m < 0) _m % 60 + 60 else _m % 60
-
-    // if (h < 0) new Clock(h + (_m / 60 % 24), m)
-    // else new Clock(h + (_m / 60 % 24), m)
+    if (hour < 0) new Clock(hour % 24 + 24, min)
+    else new Clock(hour % 24, min)
   }
 
   def apply(m: Int): Clock = Clock.apply(0, m)
@@ -26,12 +18,21 @@ object Clock {
 class Clock(val h: Int, val m: Int) {
   import Clock._
 
-  override def toString = {
-    s"${padRight(h.toString)}:${padRight(m.toString)}"
+  def +(o: Clock) = o match {
+    case c: Clock => Clock(h + c.h, m + c.m)
+  }
+
+  def -(o: Clock) = o match {
+    case c: Clock => Clock(h - c.h, m - c.m)
   }
 
   override def equals(o: Any) = o match {
     case c: Clock => c.h == h && c.m == m
     case _ => false
   }
+
+  override def toString = {
+    s"${padRight(h.toString)}:${padRight(m.toString)}"
+  }
+
 }
