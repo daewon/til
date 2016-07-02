0b10109bff754ded0aa6edcc7319353dce7f072f
daewon <daewon@apache.org>
2016-07-02 21:32:56 +0900

    add frog

--- quiz/codility/solve/frog_river_on.rb
+++ quiz/codility/solve/frog_river_on.rb
@@ -20,3 +20,17 @@ def solution(x, a)
     -1
   end
 end
+
+# you can write to stdout for debugging purposes, e.g.
+# puts "this is a debug message"
+require 'set'
+def solution(x, a)
+  set = Set.new
+  
+  a.each_with_index do |n, i|
+    set << n
+    return i if set.length == x
+  end
+  
+  -1
+end
