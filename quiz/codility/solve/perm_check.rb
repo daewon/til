e8f69b0368b7b42d307630a78fca1c9722e56ad6
daewon <daewon@apache.org>
2016-07-05 21:33:37 +0900

    check perm

--- quiz/codility/solve/perm_check.rb
+++ quiz/codility/solve/perm_check.rb
@@ -1,18 +1,12 @@
+# you can write to stdout for debugging purposes, e.g.
+# puts "this is a debug message"
+
 def solution(a)
-  # write your code in Ruby 2.2
-  return a.first == 1 ? 1 : 0 if a.one?
-
-  set = {}
-  sum, max, min = 0, -1, 9999999999999999999
-
-  a.each do |n|
-    return 0 if set.include? n
-
-    set[n] = true
-    max = n if n > max
-    min = n if n < min
-    sum += n
-  end
-
-  (sum == (min + max) * max / 2) ? 1 : 0
+  return 0 if a.uniq.size != a.size
+  
+  max = a.size  
+  sum = (1 + max) * max / 2
+  
+  array_sum = a.inject(0, &:+)  
+  sum == array_sum ? 1 : 0  
 end
