c96ba50c2c2c984bb7baa71c56fda4869bdffc4d
daewon <daewon@apache.org>
2016-07-08 21:59:33 +0900

    add pascal_triangle

---
+++ exercism/elixir/pascals-triangle/pascals_triangle.exs
@@ -0,0 +1,18 @@
+defmodule PascalsTriangle do
+  @doc """
+  Calculates the rows of a pascal triangle
+  with the given height
+  """
+  @spec rows(integer) :: [[integer]]
+  def rows(num) do
+    pascal_stream = Stream.iterate([1], fn prev ->
+      next = Enum.chunk(prev, 2, 1)
+      |> Enum.map(&Enum.sum/1)
+
+      [1, next, 1]
+      |> List.flatten
+    end)
+
+    pascal_stream |> Enum.take(num)
+  end
+end
