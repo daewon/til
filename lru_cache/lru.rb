fb063c4453bb10e8b954eddf4e5a1bb3e7d6fd24
daewon <daewon@apache.org>
2016-07-24 09:05:15 +0900

    add interface

--- lru_cache/lru.rb
+++ lru_cache/lru.rb
@@ -1,7 +1,25 @@
+class Node
+  def initialize(value)
+    @value = value
+    @prev, @next = nil
+  end
+end
+
 class LRUCache
   def initialize(size)
     @size = size
+    @map, @list = {}, nil
+  end
+
+  def get(k)
   end
 
 
+  def set(k, v)
+  end
+
+
+  def expire()
+
+  end
 end
