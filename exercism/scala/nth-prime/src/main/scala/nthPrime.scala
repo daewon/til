18754f03185f48f9e32a04195e85d90dc543bb98
daewon <daewon@apache.org>
2016-08-06 02:24:53 +0900

    replace set to list

--- exercism/scala/nth-prime/src/main/scala/nthPrime.scala
+++ exercism/scala/nth-prime/src/main/scala/nthPrime.scala
@@ -1,15 +1,16 @@
 object Prime {
   def prime(limit: Int): Int = {
     val step = 2
+
     @scala.annotation.tailrec
-    def find(count: Int, n: Int, primes: Set[Int]): Int =
+    def find(count: Int, n: Int, primes: List[Int]): Int =
       if (count == limit) n - step
       else {
-        if (primes.forall(p => n % p != 0)) find(count + 1, n + step, primes + n)
+        if (primes.forall(p => n % p != 0)) find(count + 1, n + step, n :: primes)
         else find(count, n + step, primes)
       }
 
-    find(0, 3, Set.empty[Int])
+    find(0, 3, List.empty[Int])
   }
 
   def nth(n: Int): Int = n match {
