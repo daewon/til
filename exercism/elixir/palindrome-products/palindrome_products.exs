fd1b767e24abce4f415672ae48c6ea1dcf44bcd2
daewon <daewon@apache.org>
2016-08-07 16:20:09 +0900

    fix indent

--- exercism/elixir/palindrome-products/palindrome_products.exs
+++ exercism/elixir/palindrome-products/palindrome_products.exs
@@ -6,12 +6,10 @@ defmodule Palindromes do
   @spec generate(non_neg_integer, non_neg_integer) :: map
   def generate(max, min \\ 1) do
     products = for a <- min..max, b <- a..max,
-    product = a * b,
-    str = Integer.to_string(product),
-    rev = String.reverse(str),
-    str == rev do
-      %{product => [[a, b]]}
-    end
+      product = a * b,
+      str = Integer.to_string(product),
+      rev = String.reverse(str),
+      str == rev, do: %{product => [[a, b]]}
 
     Enum.reduce(products, %{}, fn m, acc ->
       Map.merge(acc, m, fn _k, o, n -> o ++ n end)
