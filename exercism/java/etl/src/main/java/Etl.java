7fdb93738722b00836f03d0f272fcf6fb7b0b925
daewon <daewon@apache.org>
2016-09-05 01:58:33 +0900

    add java etl, suck!

--- exercism/java/etl/src/main/java/Etl.java
+++ exercism/java/etl/src/main/java/Etl.java
@@ -1,8 +1,14 @@
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.*;
 
 public class Etl {
-   public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-      return null;
-   }
+  public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+    Map<String, Integer> inverted = old
+      .entrySet()
+      .stream()
+      .flatMap(e -> e.getValue().stream().map(k -> new AbstractMap.SimpleEntry<>(k.toLowerCase(), e.getKey())))
+      .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+    return inverted;
+  }
 }
