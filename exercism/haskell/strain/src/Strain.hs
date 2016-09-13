e195b10c4878c38df54378526c3db47f4d56c583
daewon <daewon@apache.org>
2016-09-13 23:28:53 +0900

    add refactor

--- exercism/haskell/strain/src/Strain.hs
+++ exercism/haskell/strain/src/Strain.hs
@@ -1,7 +1,8 @@
 module Strain (keep, discard) where
 
 discard :: (a -> Bool) -> [a] -> [a]
-discard f a =  keep (not . f) a
+discard f =  keep $ not . f
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep f a = filter f a
+keep _ [] = []
+keep f (x:xs) = if f x then x:keep f xs else keep f xs
