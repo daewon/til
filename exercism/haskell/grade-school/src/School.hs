06ca628c11087dab20ff3fd0fafee9ff53d365a0
daewon <daewon@apache.org>
2016-10-03 21:12:00 +0900

    refactor

--- exercism/haskell/grade-school/src/School.hs
+++ exercism/haskell/grade-school/src/School.hs
@@ -3,20 +3,24 @@ module School (School, add, empty, grade, sorted) where
 import qualified Data.Map as M
 import qualified Data.List as L
 
-type Students = [String]
+type Grade = Int
+type Student = String
+type Students = [Student]
 type School = M.Map Int Students
 
-add :: Int -> String -> School -> School
+add :: Grade -> String -> School -> School
 add n name db = M.unionWith (++) db $ M.fromList [(n, [name])]
 
 empty :: School
 empty = M.empty
 
-grade :: Int -> School -> Students
+grade :: Grade -> School -> Students
 grade g db = case M.lookup g db of
   Just ls -> ls
   Nothing -> []
 
-sorted :: School -> [(Int, Students)]
-sorted m = fmap (\(n, ls) -> (n, L.sort ls)) schoolLs
-  where schoolLs = M.toList m
+sorted :: School -> [(Grade, Students)]
+sorted m = fmap sortStudents schoolLs
+  where
+    schoolLs = M.toList m
+    sortStudents (n, ls) = (n, L.sort ls)
