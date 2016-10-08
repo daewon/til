cfab71feeec3b8ca9602e386fa40404a778e1da1
daewon <daewon@apache.org>
2016-10-09 00:38:54 +0900

    add simple linked list

--- exercism/haskell/simple-linked-list/src/LinkedList.hs
+++ exercism/haskell/simple-linked-list/src/LinkedList.hs
@@ -10,28 +10,37 @@ module LinkedList
     , toList
     ) where
 
-data LinkedList a = Nil | LinkedList a
+data LinkedList a = Nil | Cons a (LinkedList a)
 
 datum :: LinkedList a -> a
-datum = undefined
+datum (Cons a _) = a
 
 fromList :: [a] -> LinkedList a
-fromList = undefined
+fromList (x : xs) = Cons x $ fromList xs
+fromList [] = Nil
 
 isNil :: LinkedList a -> Bool
-isNil = undefined
+isNil Nil = True
+isNil _ = False
 
 new :: a -> LinkedList a -> LinkedList a
-new = undefined
+new a ls@(Cons _ _) = Cons a ls
+new a Nil = Cons a Nil
 
 next :: LinkedList a -> LinkedList a
-next = undefined
+next (Cons a rest) = rest
+next Nil = Nil
 
 nil :: LinkedList a
-nil = undefined
+nil = Nil
+
+rev (Cons a rest) acc = rev rest $ Cons a acc
+rev Nil acc = acc
 
 reverseLinkedList :: LinkedList a -> LinkedList a
-reverseLinkedList = undefined
+reverseLinkedList ls@(Cons a rest) = rev ls Nil
+reverseLinkedList Nil = Nil
 
 toList :: LinkedList a -> [a]
-toList = undefined
+toList (Cons a rest) = a : (toList rest)
+toList Nil = []
