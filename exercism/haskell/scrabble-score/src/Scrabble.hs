bc4f0fcac011f1dd075c21ab6ddcceb99c26ce3d
daewon <daewon@apache.org>
2016-10-20 00:34:04 +0900

    refactoring

--- exercism/haskell/scrabble-score/src/Scrabble.hs
+++ exercism/haskell/scrabble-score/src/Scrabble.hs
@@ -1,5 +1,26 @@
 module Scrabble (scoreLetter, scoreWord) where
 
-scoreLetter = undefined
+import Data.Char
+import qualified Data.Map as M
 
-scoreWord = undefined
+scoreMap :: M.Map Char Int
+scoreMap = M.fromList $ lst >>= uncurry invert
+  where
+    invert n str = fmap (\ch -> (ch, n)) str
+    lst = [
+      (1, "AEIOULNRST"),
+      (2, "DG"),
+      (3, "BCMP"),
+      (4, "FHVWY"),
+      (5, "K"),
+      (8, "JX"),
+      (10,"QZ")]
+
+scoreLetter :: Char -> Int
+scoreLetter = findInScoreMap . toUpper
+  where
+    findInScoreMap = findWithDefault scoreMap
+    findWithDefault = flip (M.findWithDefault 0)
+
+scoreWord :: String -> Int
+scoreWord = sum . fmap scoreLetter
