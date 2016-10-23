a204956321f3ce543a6f7b3bd2a4e59eecf6940b
daewon <daewon@apache.org>
2016-10-23 15:41:18 +0900

    refactor from advice by 'abo64

--- exercism/scala/sum-of-multiples/src/main/scala/sumOfMultiple.scala
+++ exercism/scala/sum-of-multiples/src/main/scala/sumOfMultiple.scala
@@ -1,7 +1,4 @@
 object SumOfMultiples {
   def sumOfMultiples(ls: Seq[Int], limit: Int): Int =
-    ls.flatMap { n =>
-      def stream = Stream.iterate(n)(_ + n)
-      stream.takeWhile(_ < limit).toList
-    }.toSet.sum
+    ls.flatMap { n => n until limit by n }.toSet sum
 }
