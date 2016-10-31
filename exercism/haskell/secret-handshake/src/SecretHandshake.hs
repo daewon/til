31862d8cd2cba617cbfc399e5be7254c8859f420
daewon <daewon@apache.org>
2016-10-31 23:35:16 +0900

    refactor

--- exercism/haskell/secret-handshake/src/SecretHandshake.hs
+++ exercism/haskell/secret-handshake/src/SecretHandshake.hs
@@ -4,23 +4,24 @@ import Data.Bits
 import Data.Maybe
 import Data.Bool
 
-class Bitlike a where
-  toBit :: a -> Maybe Bool
+class CanBit a where
+  toBool :: a -> Maybe Bool
 
-instance Bitlike Char where
-  toBit '0' = Just False
-  toBit '1' = Just True
-  toBit _   = Nothing
+instance CanBit Char where
+  toBool '0' = Just False
+  toBool '1' = Just True
+  toBool _ = Nothing
+
+instance CanBit a => Handshake [a] where
+  interpret = reverse . fromMaybe [] . traverse toBool
 
 class Handshake a where
   interpret :: a -> [Bool]
 
 instance Handshake Int where
-  interpret n = map (testBit n) [0 .. numBits]
-    where numBits = finiteBitSize n - countLeadingZeros n - 1
-
-instance Bitlike a => Handshake [a] where
-  interpret = reverse . fromMaybe [] . traverse toBit
+  interpret n = map (testBit n) [0..numBits]
+    where
+      numBits = finiteBitSize n - countLeadingZeros n - 1
 
 handshake :: Handshake a => a -> [String]
 handshake = foldl (flip ($)) [] . zipWith (bool id) ops . interpret
