b5ead4b76a021571a08890ff60dba6d6e9384efa
daewon <daewon@apache.org>
2016-11-02 00:28:09 +0900

    abitAnd

--- exercism/haskell/allergies/src/Allergies.hs
+++ exercism/haskell/allergies/src/Allergies.hs
@@ -1,7 +1,34 @@
 module Allergies (Allergen(..), allergies, isAllergicTo) where
 
-allergies :: Int -> [Allergen]
-allergies = undefined
+import Data.Bits
+
+data Allergen = Cats
+              | Chocolate
+              | Eggs
+              | Peanuts
+              | Pollen
+              | Shellfish
+              | Strawberries
+              | Tomatoes deriving (Eq)
+
+value :: Allergen -> Int
+value Eggs = 1
+value Peanuts = 2
+value Shellfish = 4
+value Strawberries = 8
+value Tomatoes = 16
+value Chocolate = 32
+value Pollen = 64
+value Cats = 128
+
+bitAnd :: Int -> Int -> Bool
+bitAnd n m = (.&.) n m == m
 
 isAllergicTo :: Allergen -> Int -> Bool
-isAllergicTo = undefined
+isAllergicTo alg n = bitAnd n $ value alg
+
+allergies' :: [Allergen]
+allergies' = [Cats, Chocolate, Eggs, Peanuts, Pollen, Shellfish, Strawberries, Tomatoes]
+
+allergies :: Int -> [Allergen]
+allergies n = filter (\a -> isAllergicTo a n) allergies'
