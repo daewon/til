dc2b15b90d45e3897e637068d3ce486511bcf8dd
daewon <daewon@apache.org>
2016-11-11 23:11:39 +0900

    add pascal triangle

--- exercism/haskell/pascals-triangle/src/Triangle.hs
+++ exercism/haskell/pascals-triangle/src/Triangle.hs
@@ -1,4 +1,23 @@
 module Triangle (rows) where
 
+chunks :: Int -> [a] -> [[a]]
+chunks n xs
+  | length xs < n = []
+  | otherwise = ys : chunks n zs
+  where
+    (ys, zs) = (take n xs, drop 1 xs)
+
+group :: [a] -> [[a]]
+group = chunks 2
+
+row :: Int -> [Integer]
+row 0 = []
+row 1 = [1]
+row 2 = [1, 1]
+row n = [1] ++ current ++ [1]
+  where
+    current = fmap sum $ group prev
+    prev = row $ n - 1
+
 rows :: Int -> [[Integer]]
-rows = undefined
+rows limit = [row n | n <- [1..limit]]
