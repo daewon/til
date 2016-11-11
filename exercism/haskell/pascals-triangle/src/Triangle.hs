83e988531fd9778eff356ffbb6d3a36f9fa17fca
daewon <daewon@apache.org>
2016-11-12 00:07:05 +0900

    iterate version

--- exercism/haskell/pascals-triangle/src/Triangle.hs
+++ exercism/haskell/pascals-triangle/src/Triangle.hs
@@ -1,23 +1,10 @@
 module Triangle (rows) where
 
-chunks :: Int -> [a] -> [[a]]
-chunks n xs
-  | length xs < n = []
-  | otherwise = ys : chunks n zs
-  where
-    (ys, zs) = (take n xs, drop 1 xs)
-
-group :: [a] -> [[a]]
-group = chunks 2
-
-row :: Int -> [Integer]
-row 0 = []
-row 1 = [1]
-row 2 = [1, 1]
-row n = [1] ++ current ++ [1]
-  where
-    current = fmap sum $ group prev
-    prev = row $ n - 1
-
 rows :: Int -> [[Integer]]
-rows limit = [row n | n <- [1..limit]]
+rows limit
+  | limit < 0 = []
+  | otherwise = takeN triangles
+  where
+    triangles = iterate nextTriangle [1]
+    takeN = take limit
+    nextTriangle ls = fmap (uncurry (+)) $ zip (0 : ls) (ls ++[0])
