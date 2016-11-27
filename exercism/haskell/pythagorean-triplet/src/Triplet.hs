79d8fe284686597822f0fe2ccfd926ec3959a37d
daewon <daewon@apache.org>
2016-11-27 15:21:30 +0900

    refactor

--- exercism/haskell/pythagorean-triplet/src/Triplet.hs
+++ exercism/haskell/pythagorean-triplet/src/Triplet.hs
@@ -5,12 +5,12 @@ import Data.List (sort)
 isPythagorean :: (Int, Int, Int) -> Bool
 isPythagorean (a', b', c') = a * a + b * b == c * c
   where
-    a : b : c : [] = sort [a', b', c']
+    [a, b, c] = sort [a', b', c']
 
 mkTriplet :: Int -> Int -> Int -> (Int, Int, Int)
 mkTriplet a' b' c' = (a, b, c)
   where
-    a : b : c : [] = sort [a', b', c']
+    [a, b, c] = sort [a', b', c']
 
 pythagoreanTriplets :: Int -> Int -> [(Int, Int, Int)]
 pythagoreanTriplets min max =
