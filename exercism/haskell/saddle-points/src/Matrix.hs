de194ddf34a7ab6814522d4ddbca659dbda22174
daewon <daewon@apache.org>
2016-11-27 15:18:17 +0900

    refactor

--- exercism/haskell/saddle-points/src/Matrix.hs
+++ exercism/haskell/saddle-points/src/Matrix.hs
@@ -1,6 +1,6 @@
 module Matrix (saddlePoints) where
 
-import Data.Array (Array, bounds, assocs, (!))
+import Data.Array (Array, (!), assocs, bounds)
 
 type Point = (Int, Int)
 
@@ -8,15 +8,11 @@ saddlePoints :: Array Point Int -> [Point]
 saddlePoints matrix =
   let
     ((minRow, minCol), (maxRow, maxCol)) = bounds matrix
-    maxRows = [ maximum [ matrix ! (x, y)
-      | y <- [minCol .. maxCol] ]
-      | x <- [minRow .. maxRow] ]
-    minCols = [ minimum [ matrix ! (x, y)
-      | x <- [minRow .. maxRow] ]
-      | y <- [minCol .. maxCol] ]
+    maxRows = [ maximum [ matrix ! (x, y) | y <- [minCol .. maxCol] ] | x <- [minRow .. maxRow] ]
+    minCols = [ minimum [ matrix ! (x, y) | x <- [minRow .. maxRow] ] | y <- [minCol .. maxCol] ]
     findPoints (p@(r,c), v) acc =
-        if maxRows !! r == v && minCols !! c == v
-          then p:acc
-          else acc
+      if maxRows !! r == v && minCols !! c == v
+      then p:acc
+      else acc
   in
     foldr findPoints [] $ assocs matrix
