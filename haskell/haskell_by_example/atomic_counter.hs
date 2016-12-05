c506acc5685b17242e409234b9fd33ded093010d
daewon <daewon@apache.org>
2016-12-05 23:58:05 +0900

    add atomic counter

---
+++ haskell/haskell_by_example/atomic_counter.hs
@@ -0,0 +1,14 @@
+import Control.Monad
+import Control.Concurrent
+import Control.Concurrent.STM
+
+main = do
+    ops <- atomically $ newTVar 0
+    forM_ [0..49] $ \_ -> do
+        forkIO . forever $ do
+            atomically $ modifyTVar ops (+1)
+            threadDelay 100
+
+    threadDelay 1000000
+    opsFinal <- atomically $ readTVar ops
+    putStrLn $ "ops: " ++ show opsFinal
