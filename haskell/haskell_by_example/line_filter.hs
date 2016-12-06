a350161273766ab0eabc689ecadae111f652538b
daewon <daewon@apache.org>
2016-12-06 18:34:48 +0900

    add example

---
+++ haskell/haskell_by_example/line_filter.hs
@@ -0,0 +1,3 @@
+import Data.Char
+
+main = interact $ fmap toUpper
---
+++ haskell/haskell_by_example/number_parsing.hs
@@ -0,0 +1,6 @@
+main = do
+    print (read "1.234" :: Double)
+    print (read "123"   :: Int)
+    print (read "0x1c8" :: Int)
+    print (read "wat"   :: Int)
+
