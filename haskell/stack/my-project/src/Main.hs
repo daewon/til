b6123d10e33fe713ec67ec300cf55f32864553a8
daewon <daewon@apache.org>
2016-12-09 09:12:54 +0900

    building rest server in haskell

--- haskell/stack/my-project/src/Main.hs
+++ haskell/stack/my-project/src/Main.hs
@@ -1,13 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
+import GHC.Generics
+import Web.Scotty
+import Data.Monoid ((<>))
 import System.IO (readFile)
 import Data.Time (getCurrentTime)
+import Data.Aeson
+
+data User = User
+  {
+    userId :: Int,
+    userName :: String
+  } deriving (Show, Eq)
+
+instance ToJson User
+instance FromJSON User
 
 printConfig = do
   contents <- readFile "stack.yaml"
   putStrLn contents
 
+route :: ScottyM ()
+route = do
+  get "/hello/:name" $ do
+    name <- param "name"
+    text ("hello " <> name <> "!")
+
+bob :: User
+bob = User { userId = 1, userName = "bob" }
+
+jenny :: User
+jenny = User { userId = 1, userName = "jenny" }
+
+allUsers :: [User]
+allUsers = [bob, jenny]
+
+hello :: ActionM ()
+hello = do
+  text "hello world"
+
 main :: IO ()
 main = do
-  time <- getCurrentTime
-  putStrLn $ show time
+  putStrLn "hello world"
+  scotty 3000 $ route
